383d33cff6869680c9ef7a9a949e7d3f10ed5695
KunalNasa <kunalnasa.dev@gmail.com>
2025-04-22 21:49:40 +0530

    new message after conflict

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("New file created");
 
-console.log("From local branch");console.log("Add conflict from main branch");
+console.log("From local branch");
+console.log("Add conflict from main branch");
+console.log("I think I merged conflict");
